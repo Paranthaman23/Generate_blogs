0564a8b7106a197e760da5cec835b5b95528b45d
Paranthaman23 <145899283+Paranthaman23@users.noreply.github.com>
2024-02-02 22:08:00 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,10 @@
-import os
 import streamlit as st
 from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
 
 def getLLamaresponse(input_text,no_words,blog_style):
     
-    llm=CTransformers(model= "models/llama-2-7b-chat.ggmlv3.q8_0.bin",
+    llm=CTransformers(model= 'models/llama-2-7b-chat.ggmlv3.q8_0.bin',
                       model_type ='llama',
                       config={'max_new_tokens':256,
                               'temperature':0.01})
